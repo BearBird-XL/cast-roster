0d4422f99093784f1736964d3a46e0dff1a4fa1a
xionglei <xionglei@jiuqi.com.cn>
2021-07-29 17:42:49 +0800

    add: 初始化项目

---
+++ cast-roster-dao/src/main/java/xyz/xionglei/dao/IPerformerDao.java
@@ -0,0 +1,9 @@
+package xyz.xionglei.dao;
+
+import org.springframework.data.repository.Repository;
+import xyz.xionglei.entity.Performer;
+
+public interface IPerformerDao extends Repository<Performer, Long> {
+
+
+}
