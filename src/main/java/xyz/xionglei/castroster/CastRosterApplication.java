1824da1c317da9de3001ff54aee84c77b7b5eea8
xionglei <xionglei@jiuqi.com.cn>
2021-07-29 17:08:16 +0800

    add: init git repository

---
+++ src/main/java/xyz/xionglei/castroster/CastRosterApplication.java
@@ -0,0 +1,13 @@
+package xyz.xionglei.castroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastRosterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastRosterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/xionglei/castroster/CastRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xionglei.castroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastRosterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
