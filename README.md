494854937c21ab12c41014db7181968e0547a60c
lei <xionglei1614@163.com>
2021-08-06 23:30:04 +0800

    add(cast-roster-worm): 添加warm模块用于重网络中获取资源

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cast-roster
 readme
+
+角色演员作品集
+
+### cast-roster-admin 后管后端 
+### cast-roster-admin-web 后端前端
+### cast-roster-dao 持久层代码
+### cast-roster-init 项目初始化执行代码
+### cast-roster-service 业务逻辑层代码
+### cast-roster-test 测试代码
+### cast-roster-web 普通前端代码
+### cast-roster-worm 爬虫获取网络数据代码
---
+++ cast-roster-worm/src/main/java/xyz/xionglei/worm/WormApp.java
@@ -0,0 +1,12 @@
+package xyz.xionglei.worm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WormApp.class, args);
+    }
+}
